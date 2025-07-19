03b9adbc56bccdb0c465f92c76b43c745ca5e433
_wiktoreq_12 <wiktoreq12@op.pl>
2025-07-19 17:55:37 +0200

    Initial commit

---
+++ wiktoreqxyz-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { LoadningScreen } from './components/LoadingScreen'
+
+function App() {
+  return (
+    <>
+      <LoadningScreen />
+    </>
+  )
+}
+
+export default App
